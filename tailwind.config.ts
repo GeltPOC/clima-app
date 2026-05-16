13a9ab6b834ac080ced5affd9d3431c33e7d0dc3
GeltTech <tech@gelt.com>
2026-05-17 00:00:24 +0200

    feat: tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss'
+const config: Config = {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 8s linear infinite',
+        'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'bounce-slow': 'bounce 3s infinite',
+      }
+    }
+  },
+  plugins: []
+}
+export default config
