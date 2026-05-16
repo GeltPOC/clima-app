f2a7f642f3682b946f01a7384085258482fac503
GeltTech <tech@gelt.com>
2026-05-17 00:00:34 +0200

    feat: src/features/weather/types/weather.types.ts

---
+++ src/features/weather/types/weather.types.ts
@@ -0,0 +1,33 @@
+export interface GeoResult {
+  id?: number
+  name: string
+  latitude: number
+  longitude: number
+  country: string
+  country_code?: string
+  admin1?: string
+  admin2?: string
+}
+
+export interface WeatherCurrent {
+  temperature_2m: number
+  relative_humidity_2m: number
+  apparent_temperature: number
+  weather_code: number
+  wind_speed_10m: number
+  wind_direction_10m: number
+}
+
+export interface WeatherDaily {
+  time: string[]
+  temperature_2m_max: number[]
+  temperature_2m_min: number[]
+  weather_code: number[]
+}
+
+export interface WeatherData {
+  current: WeatherCurrent
+  daily: WeatherDaily
+  timezone: string
+  timezone_abbreviation: string
+}
